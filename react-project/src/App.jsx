1ba282c0f5039d504aa49384e57ef9fbb1c9aecb
Richard <richardfadhilah120@gmail.com>
2025-08-30 08:11:27 +0700

    Learn react and make project frame

---
+++ react-project/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="../../public/hero-img.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+      </div>
+    </main>
+  );
+};
+
+export default App;
---
+++ react-project/src/components/Search.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Search = (props) => {
+  return <div className="text-3xl text-white">{props.searchTerm}</div>;
+};
+
+export default Search;
